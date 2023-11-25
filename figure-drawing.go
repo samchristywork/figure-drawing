7311f471e8b7d5334ed6a76c3205cc41bddab126
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-24 22:35:51 -0600

    chore: Add error handling and reporting
    Previously, the application would not report what port it was listening
    on, or any errors that occurred.

--- figure-drawing.go
+++ figure-drawing.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func serve() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	http.ListenAndServe(":8000", nil)
+	fmt.Println("Listening on port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
