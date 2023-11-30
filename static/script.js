5173f5886b25bea0a231c8e79030540d0af2e123
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-29 23:06:24 -0600

    feat: Change pan sensitivity with zoom
    When you zoom close, the pan should happen more slowly, and vice versa
    as well.

--- static/script.js
+++ static/script.js
@@ -159,13 +159,13 @@ function render() {
 
 document.addEventListener("keydown", function(e) {
   if (e.key == "w") {
-    viewportY -= 10;
+    viewportY -= 10 * viewportScale * 10;
   } else if (e.key == "a") {
-    viewportX -= 10;
+    viewportX -= 10 * viewportScale * 10;
   } else if (e.key == "s") {
-    viewportY += 10;
+    viewportY += 10 * viewportScale * 10;
   } else if (e.key == "d") {
-    viewportX += 10;
+    viewportX += 10 * viewportScale * 10;
   } else if (e.key == "q") {
     viewportScale *= 1.1;
   } else if (e.key == "e") {
